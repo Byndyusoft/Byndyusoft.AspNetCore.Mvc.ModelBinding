fa2f24836f7d002b962709f3b8fd8f3f2ebacf63
Rena88 <foxcoolman@mail.ru>
2023-08-08 17:28:05 +0500

    Удалил ненужный хэлпер класс

--- tests/PerformanceTests/Helpers/BenchmarkTestInstance.cs
+++ tests/PerformanceTests/Helpers/BenchmarkTestInstance.cs
@@ -1,5 +1,8 @@
-﻿using System.Net.Http;
+﻿using System.IO;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Byndyusoft.IntegrationTests;
 using PerformanceTests.Files;
 
 namespace PerformanceTests.Helpers
@@ -11,13 +14,13 @@ namespace PerformanceTests.Helpers
 
         public BenchmarkTestInstance(string postMethodPrefix)
         {
-            _httpClient = BenchmarkTestHelper.GetHttpClient();
+            _httpClient = GetHttpClient();
             _postMethodPrefix = postMethodPrefix;
         }
         
         public async Task<T> TestOldWay<T>(TestFileSize testFileSize)
         {
-            return await BenchmarkTestHelper.CallApiMethod<T>(
+            return await CallApiMethod<T>(
                 _httpClient,
                 _postMethodPrefix,
                 testFileSize,
@@ -26,11 +29,58 @@ namespace PerformanceTests.Helpers
 
         public async Task<T> TestNewWay<T>(TestFileSize testFileSize)
         {
-            return await BenchmarkTestHelper.CallApiMethod<T>(
+            return await CallApiMethod<T>(
                 _httpClient,
                 _postMethodPrefix,
                 testFileSize,
                 isNew: true);
+        }
+
+        public static HttpClient GetHttpClient()
+        {
+            var apiFixture = new ApiFixture();
+            return apiFixture.CreateClient();
+        }
+
+        public static async Task<T> CallApiMethod<T>(
+            HttpClient httpClient,
+            string postMethodPrefix,
+            TestFileSize testFileSize,
+            bool isNew)
+        {
+            using var multipartFormDataContent = GetMultipartFormDataContent(testFileSize);
+            var postMethodSuffix = isNew ? "New" : "Old";
+            var postMethodName = $"{postMethodPrefix}{postMethodSuffix}";
+            using var httpResponseMessage = await httpClient.PostAsync(postMethodName, multipartFormDataContent);
+            var result = await httpResponseMessage.Content.ReadFromJsonAsync<T>();
+            return result!;
+        }
+
+        private static MultipartFormDataContent GetMultipartFormDataContent(TestFileSize testFileSize)
+        {
+            var multipartFormDataContent = new MultipartFormDataContent
+            {
+                { new StringContent("Ivan"), "Name" },
+                { new StringContent("35"), "Age" }
+            };
+
+            foreach (var testFileInfo in FileGenerator.GetTestFiles(testFileSize))
+            {
+                var streamContent = CreateStreamContent(testFileInfo.Content);
+                multipartFormDataContent.Add(streamContent, "Files", testFileInfo.FileName);
+            }
+
+            return multipartFormDataContent;
+        }
+
+        public static StreamContent CreateStreamContent(byte[] fileContent)
+        {
+            var memoryStream = new MemoryStream(fileContent);
+            var streamContent = new StreamContent(memoryStream);
+
+            streamContent.Headers.ContentLength = fileContent.Length;
+
+            return streamContent;
         }
     }
 }
